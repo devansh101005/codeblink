3f09c2822ea5610da84144e99f189048522036a1
Devansh Pandey <devanshpandeyji4321@gmail.com>
2025-09-16 03:13:55 +0530

    improved accuracy for indentation

--- txt-ss/extension.js
+++ txt-ss/extension.js
@@ -10,93 +10,6 @@ const fs=require("fs");
 /** 
 * @param {vscode.ExtensionContext} context
  */
-// function activate(context) {
-
-// 	// Use the console to output diagnostic information (console.log) and errors (console.error)
-// 	// This line of code will only be executed once when your extension is activated
-// 	console.log('Congratulations, your extension "txt-ss" is now active!');
-
-// 	// The command has been defined in the package.json file
-// 	// Now provide the implementation of the command with  registerCommand
-// 	// The commandId parameter must match the command field in package.json
-// 	const disposable = vscode.commands.registerCommand('txt-ss.helloWorld', function () {
-// 		// The code you place here will be executed every time your command is executed
-
-// 		// Display a message box to the user
-// 		//vscode.window.showInformationMessage('Hello World from txt-ss!');
-//        const editor = vscode.window.activeTextEditor;
-//        if (editor) {
-//       editor.edit(editBuilder => {
-//     editBuilder.insert(editor.selection.active, "Hello from my first extension!");
-//   });
-// }
-
-
-
-// 	});
-
-// 	context.subscriptions.push(disposable);
-// }
-
-//Writing that same function with some changes
-
-
-
-
-
-// function activate(context) {
-//     let disposable = vscode.commands.registerCommand('extension.pasteScreenshotText',
-// 		async function () {
-//         const editor = vscode.window.activeTextEditor;
-//          if (!editor) return;
-
-//       const tmpFile = path.join(os.tmpdir(), "vscode_clip.png");
-//       exec(
-//         `powershell -command "Add-Type -AssemblyName System.Windows.Forms; if ([Windows.Forms.Clipboard]::ContainsImage()) { [Windows.Forms.Clipboard]::GetImage().Save('${tmpFile}'); }"`,
-//         (err) => {
-//           if (err) {
-//             vscode.window.showErrorMessage("No image in clipboard!");
-//             return;
-//           }
-
-//           vscode.window.showInformationMessage("Running OCR...");
-
-
-//           Tesseract.recognize(tmpFile, "eng")
-//             .then(({ data: { text } }) => {
-				
-//               editor.edit((editBuilder) => {
-//                 editBuilder.insert(editor.selection.active, text);
-//               });
-// 			   vscode.window.showInformationMessage("OCR complete!");
-//             })
-//             .catch((err) => {
-//               vscode.window.showErrorMessage("OCR failed: " + err.message);
-//             });
-//         }
-//       );
-//     }
-//   );
-
-
-
-    
-
-//     context.subscriptions.push(disposable);
-// }
-
-
-
-//  function deactivate() {}
-
-//  module.exports = {
-//  	activate,
-//  	deactivate
-//  }
-
-
-
-
 
 function activate(context) {
   // 1. Create a dedicated Output Channel
@@ -130,8 +43,15 @@ function activate(context) {
               outputChannel.show(true); // Auto-show logs
               outputChannel.appendLine("=== OCR Process Started ===");
               outputChannel.appendLine("Image file: " + tmpFile);
+             
+
 
               try {
+
+                const params={
+                  preserve_interword_spaces:'1',
+                };
+
                 const result = await Tesseract.recognize(tmpFile, "eng", {
                   logger: (m) => {
                     outputChannel.appendLine(`[OCR] ${m.status} (${Math.round(m.progress * 100)}%)`);
@@ -142,6 +62,8 @@ function activate(context) {
                       });
                     }
                   },
+                  tessedit_pageseg_mode: Tesseract.PSM.SINGLE_BLOCK,
+                  ...params
                 });
 
                 const text = result.data.text;
